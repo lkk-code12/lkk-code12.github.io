0eb7bca5d51bed9fa7b735eb6b088e70a5fcabc2
lkk-code12 <lwinkoko1261996@gmail.com>
2022-01-10 14:42:31 +0630

    Second Commit

---
+++ Z2P4/JavaScript/exercise.js
@@ -0,0 +1,5 @@
+function show(){
+    alert('Hello Javascript')
+    document.write('Hi JS')
+    console.log('Hi Javascript')
+}
---
+++ Z2P4/JavaScript/show.js
@@ -0,0 +1,5 @@
+function show(){
+        alert('hello')
+        document.write('Hi JavaScript')
+        console.log('Hi JS')
+    }
