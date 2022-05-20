2ca50472dae2f72dc8ca19956b28b8a864c6ad12
lkk-code12 <lwinkoko1261996@gmail.com>
2022-05-20 11:23:26 +0630

    first commit

---
+++ CodeLabs/JavaScript/script.js
@@ -0,0 +1,29 @@
+console.log("Hello JavaScript from external");
+
+document.write("Hello JavaScript");
+
+alert("Hi JavaScript");
+
+confirm("Do you like JavaScript?");
+
+prompt("Type any number : ");
+
+//Do & Don't
+// // user name
+// userName / user_name (should be written);
+// UserName /1Number / !@#$%^&*()-+-*/= (shouldn't be written);
+
+// JavaScript Data Type
+//1. String => "" , '', ``  => "s";
+//2. integer => 10 100 1000 123456
+//3. boolean => true , false
+//4. float => 1.0 1.23 1.535 1312.
+
+console.log(typeof("Hello JavaScript"));
+
+//variable declaration
+//let, var, const
+
+let name = "Lwin Ko Ko";
+name = "Aung Aung"
+console.log(name);
