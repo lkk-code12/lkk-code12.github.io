06a85b9722e7396e7c656437e8bd1668dce5709d
Lwin Ko Ko <lwinkoko1261996@gmail.com>
2025-06-20 10:25:01 +0630

    pc sp done

--- znisa/assets/js/script.js
+++ znisa/assets/js/script.js
@@ -1,6 +1,7 @@
 $().ready(() => {
     $("#menu_btn_block").click(()=>{
         console.log("success");
+        $("body").toggleClass("scroll_block");
         $(".menu_btn_one").toggleClass("bar_one_active");
         $(".menu_btn_two").toggleClass("bar_two_active");
         $(".menu_btn_three").toggleClass("bar_three_active");
