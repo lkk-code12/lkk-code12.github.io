5547482577fc65895500d3b667696efb13df6264
Lwin Ko Ko <lwinkoko1261996@gmail.com>
2025-08-13 20:53:25 +0630

    header footer

--- coosy/assets/js/jquery.common.js
+++ coosy/assets/js/jquery.common.js
@@ -1,8 +1,8 @@
 // function call header and footer
 // ======================
 $(function () {
-  $("#header").load("/include/_header.html");
-  $("#footer").load("/include/_footer.html");
+  $("#header").load("../../include/_header.html");
+  $("#footer").load("../../include/_footer.html");
 });
 
 // scroll event for header
